03e12c5d509968156536acea79388408174019d6
Jonay <alu0101347141@ull.edu.es>
2022-04-24 01:16:12 +0100

    Actualizacion de tests

--- tests/notas.spec.ts
+++ tests/notas.spec.ts
@@ -1,18 +1,2 @@
-import 'mocha';
-import {expect} from 'chai';
-import {Nota} from '../src/notas';
-import {User} from '../src/notas';
-import {NotasDB} from '../src/notas';
 
-describe('Notas:', () => {
-  it('Debe existir una clase para representar una nota', () => {
-    expect(Nota).to.exist;
-  });
-  it('Debe existir una clase para representar un usuario', () => {
-    expect(User).to.exist;
-  });
-  it('Debe existir una clase para representar una base de datos de notas', () => {
-    expect(NotasDB).to.exist;
-  });
-});
 
